722ffbedc26dd0adfc84ec0f333477abd252b150
Daniyal Ahmed <Daniyal_AHMED92@OUTLOOK.COM>
2016-09-05 08:36:20 +0500

    New Routers Added

--- routers/eventsLogR.js
+++ routers/eventsLogR.js
@@ -1,11 +1,11 @@
 	var express		=require('express'); 
  			var app			=express();    
  			var crudR		=express.Router(); 
- 			var crudC		=require('./../controllers/pinStateC.js')();
+ 			var crudC		=require('./../controllers/eventsLogC.js')();
  			crudR.route('/')
  				.get(crudC.get)
  				.post(crudC.post);
- 			crudR.route('/:id/:userid')
+ 			crudR.route('/:piid/:userid')
  				.get(crudC.getOne)
  				.put(crudC.put)
  				.delete(crudC.delete) 
